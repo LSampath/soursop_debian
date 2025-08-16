153c0577d978ebfea63b6de79924363017165be4
LSampath <lsampath210@gmail.com>
2025-08-16 11:56:33 +0530

    included deb file

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-set -e
-set -x
+set -e      # immediately exit of any command fails
+set -x      # print each command before running
 
 # Clean previous builds
 rm -rf dist deb_dist soursop.egg-info soursop-*.tar.gz
@@ -9,8 +9,11 @@ rm -rf dist deb_dist soursop.egg-info soursop-*.tar.gz
 # Build .deb package
 python3 setup.py --command-packages=stdeb.command bdist_deb
 
+# Copy final .deb into root
+sudo mv deb_dist/python3-soursop_*_all.deb soursop_0.1.deb
+
 # Install the package
-sudo dpkg -i deb_dist/python3-soursop_*_all.deb
+sudo dpkg -i soursop_0.1.deb
 
 # Clean new build files
 # comment out this line if you want to keep the build files
